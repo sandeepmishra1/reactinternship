223644cd431f4f1a073cb7b7a29b3919e3586f6e
sandeepmishra1 <58456730+sandeepmishra1@users.noreply.github.com>
2020-07-15 20:17:59 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Myprops from './Class3';
+import Arrrayexmp from './Class4/Class4';
+
 class Examp extends React.Component{
     
     constructor(props){
@@ -7,7 +9,9 @@ class Examp extends React.Component{
         this.state={
             fnum:0,
         snum:0,
-        res:0
+        res:0,
+        twiceofsum:0,
+        resarr:[]
         }
 
     }
@@ -16,20 +20,47 @@ class Examp extends React.Component{
     }
     add=()=>{
         
+        
         this.setState({
-            res:parseInt(this.state.fnum)+parseInt(this.state.snum)
+            res:parseInt(this.state.fnum)+parseInt(this.state.snum),
+        fnum:0,
+        snum:0,
+        
+        
+        },function(){
+            var k=this.state.resarr;
+        k.push(this.state.res);
+            this.setState({
+            twiceofsum:this.state.res*2,
+            resarr:k
+            })
+
         }); 
+        
     }
     sub=()=>{
         
         this.setState({
             res:parseInt(this.state.fnum)-parseInt(this.state.snum)
+        },function(){
+            var k=this.state.resarr;
+            k.push(this.state.res);
+            this.setState({
+                resarr:k
+            })
+
         }); 
     }
     mul=()=>{
         
         this.setState({
             res:parseInt(this.state.fnum)*parseInt(this.state.snum)
+        },function(){
+            var k=this.state.resarr;
+            k.push(this.state.res);
+            this.setState({
+                resarr:k
+            })
         }); 
     }
 render(){
@@ -37,11 +68,11 @@ render(){
         <div >
             
                 ENTER FIRST NUBMER:<br/>
-                    <input type="text" name="fnum" onChange={this.handel} placeholder="Enter Your First Number"/>
+                    <input type="text" name="fnum" value={this.state.fnum} onChange={this.handel} placeholder="Enter Your First Number" autoComplete="off"/>
                     <br/>
                 
                 ENTER SECOND NUMBER:<br/>
-                    <input type="text"  name="snum" onChange={this.handel} placeholder="Enter Your Second Number"/><br/><br/>
+                    <input type="text"  name="snum" value={this.state.snum} onChange={this.handel} placeholder="Enter Your Second Number" autoComplete="off" /><br/><br/>
                 
                 <button onClick={this.add}>ADDITION</button>&nbsp;&nbsp;
                 
@@ -55,6 +86,15 @@ render(){
             SECOND NUMBER VALUE={this.state.snum}
             <br/>
             RESULT={this.state.res}
+            <br/>
+            <h1 style={{color:'red'}}>ALL RESULT IN ARRAY={this.state.resarr.toString()}</h1>
+            <br/>
+            TWICE OF NO IS={this.state.twiceofsum}
+            <Myprops vala={this.state.fnum}
+            valb={this.state.snum}
+            
+            />
+        <Arrrayexmp/>
         </div>
     )
 }
